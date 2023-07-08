28005ae28dde1a50520f6eb57b26f845c641ab66
Kamilplhh <Kamil_PLHH@wp.pl>
2023-07-08 21:03:41 +0200

    Both task parts done

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php 
 $file = "https://gist.githubusercontent.com/miisieq/379bb51bb376b2fd597d19281a7bbff6/raw/573dd374139cc72ffb555fef80af8263d2d26cd2/php_internship_data.csv";
-
 $dataArray = array_map("str_getcsv", file($file));
 
 //name section
@@ -10,8 +9,14 @@ $nameCount = array_count_values($name);
 arsort($nameCount);
 $topNames = array_slice($nameCount, 0, 10);
 
-print_r($topNames);
-echo '<br>';
+$topNames = array_combine(array_map('mb_strtolower', array_keys($topNames)), $topNames);
+$topNames = array_combine(array_map('ucfirst', array_keys($topNames)), $topNames);
+
+$x = 1;
+foreach($topNames as $key => $row){
+    echo "$x. $key appears $row times<br>";
+    $x++;
+}
 
 //
 //date section (bonus)
@@ -28,4 +33,8 @@ $dateCount = array_count_values($dateArray);
 arsort($dateCount);
 $topDates = array_slice($dateCount, 0, 10);
 
-print_r($topDates);
+$y = 1;
+foreach($topDates as $key => $row){
+    echo "$y. $key appears $row times<br>";
+    $y++;
+}
